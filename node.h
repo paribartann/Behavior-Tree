dad812a9600fe4ca6f4f5b9dd0d6739ae90d8568
Paribartan Dhakal <paribartandhakal@Paribartans-MacBook-Pro.local>
2019-06-13 12:19:23 -0500

    added GNU copyright

--- node.h
+++ node.h
@@ -1,4 +1,20 @@
-/* taken help from geek foo geeks */
+/* 
+#Copyright (C) <June, 2019>  <Paribartan Dhakal>
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as
+#published by the Free Software Foundation, either version 3 of the
+#License, or (at your option) any later version.
+
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+*/
+
+
+/* taken help from geek for geeks for Node structure */
 
 
 #ifndef NODE_H
--- pseudoDomain.h
+++ pseudoDomain.h
@@ -1,4 +1,20 @@
 
+
+/* 
+#Copyright (C) <June, 2019>  <Paribartan Dhakal, Bala Tripura Sundari Yerrapothu>
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as
+#published by the Free Software Foundation, either version 3 of the
+#License, or (at your option) any later version.
+
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+*/
+
 #ifndef PSEUDO_DOMAIN_H
 #define PSEUDO_DOMAIN_H
 #include<string>
